efb185472b471a6d86fa582d1c61a1b39c752117
MysticNL <MysticNL@users.noreply.github.com>
2020-02-21 13:34:05 +0100

    wazabi

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+    
+    include "./view/Header/navbar.php";
+?>
---
+++ view/Header/navbar.php
@@ -0,0 +1,54 @@
+<?php
+    echo "<!DOCTYPE html>";
+    echo  "<html>";
+    echo "<header> </header>";
+    echo "<style>
+    ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        overflow: hidden;
+        background-color: MediumSeaGreen;
+      }
+      
+      li {
+        float: left;
+      }
+      
+      li a {
+        display: block;
+        color: white;
+        text-align: center;
+        padding: 14px 16px;
+        text-decoration: none;
+      }
+      
+    
+      li a:hover {
+        background-color: darkgreen;
+      }
+    </style>";
+    echo "<body>
+        
+    <ul>
+    <li><a href='#home'>Home</a></li>
+    <li><a href='#news'>Products</a></li>
+    <li><a href='#contact'>About</a></li>
+    <li><a href='#about'>Contact</a></li>
+    </ul>
+    </body>";
+    
+    
+   
+   
+    
+    
+   echo "</html>";
+
+
+
+
+
+
+
+?>
